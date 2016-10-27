e3aee023ad11c5629fbaf5101099f626496bf5aa
yogeshbalan <balanyogesh@gmail.com>
2016-10-27 18:56:19 +0530

    v0.1

--- app/src/main/java/android/barcodedemo/ScannerActivity.java
+++ app/src/main/java/android/barcodedemo/ScannerActivity.java
@@ -2,20 +2,30 @@ package android.barcodedemo;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+
+import java.util.Random;
 
 import me.dm7.barcodescanner.zbar.Result;
 import me.dm7.barcodescanner.zbar.ZBarScannerView;
 
-public class ScannerActivity extends BaseActivity implements ZBarScannerView.ResultHandler{
+public class ScannerActivity extends BaseActivity implements ZBarScannerView.ResultHandler {
 
     private ZBarScannerView mScannerView;
+    private View loadingLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+        loadingLayout = findViewById(R.id.loading_layout);
+        loadingLayout.setVisibility(View.GONE);
         setupToolbar();
         getSupportActionBar().setTitle("Bring the QR code");
         ViewGroup contentFrame = (ViewGroup) findViewById(R.id.content_frame);
@@ -40,6 +50,7 @@ public class ScannerActivity extends BaseActivity implements ZBarScannerView.Res
     public void handleResult(Result result) {
         Toast.makeText(this, "Contents = " + result.getContents() +
                 ", Format = " + result.getBarcodeFormat().getName(), Toast.LENGTH_SHORT).show();
+        startLoadingAnimation();
         // Note:
         // * Wait 2 seconds to resume the preview.
         // * On older devices continuously stopping and resuming camera preview can result in freezing the app.
@@ -50,6 +61,35 @@ public class ScannerActivity extends BaseActivity implements ZBarScannerView.Res
             public void run() {
                 mScannerView.resumeCameraPreview(ScannerActivity.this);
             }
-        }, 2000);
+        }, 500);
     }
+
+    public void startLoadingAnimation() {
+        loadingLayout.setVisibility(View.VISIBLE);
+        ImageView imageView = (ImageView) findViewById(R.id.load_img);
+        GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(imageView);
+        //Glide.with(this).load(R.raw.loading_two).into(imageViewTarget);
+        int randomNumber = randInt(0, 100);
+        if (randomNumber < 25) {
+            Glide.with(this).load(R.raw.loading_one).into(imageViewTarget);
+        } else if (randomNumber < 50 && randomNumber >= 25) {
+            Glide.with(this).load(R.raw.loading_two).into(imageViewTarget);
+        } else if (randomNumber < 75 && randomNumber >= 50) {
+            Glide.with(this).load(R.raw.loading_three).into(imageViewTarget);
+        } else {
+            Glide.with(this).load(R.raw.loading_four).into(imageViewTarget);
+        }
+    }
+
+    public static int randInt(int min, int max) {
+        Random rand = new Random();
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
+        return randomNum;
+    }
+
+
 }
